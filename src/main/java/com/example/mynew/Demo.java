13e331f6b5b90c0f0bde73c8c6ba6f6865017493
chetanMager51 <sonumagar4951@gmail.com>
2025-09-17 20:29:32 +0530

    method added

---
+++ src/main/java/com/example/mynew/Demo.java
@@ -0,0 +1,12 @@
+package com.example.mynew;
+
+public class Demo {
+	
+	//this add for demo only
+	
+	
+	public static void main(String args[]) {
+	System.out.println("this is demo class ....");
+	}
+
+}
